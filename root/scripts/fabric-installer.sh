cc28ccdca3200bd0816f2a6be313f4908f7b7c04
DragonCrafted87 <24697493+DragonCrafted87@users.noreply.github.com>
2020-12-19 18:22:39 -0600

    change distutils logger

--- root/scripts/fabric-installer.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-cd /mnt/minecraft
-java -jar fabric-installer.jar server -downloadMinecraft
-rm fabric-installer.jar
-exec java -Xms4G -Xmx4G -jar fabric-server-launch.jar --nogui --forceUpgrade
--- root/scripts/start-server.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-cd /mnt/minecraft
-exec java -Xms4G -Xmx4G -jar fabric-server-launch.jar --nogui
