520afb0345624fd08b35b8f2439cb88f1628a205
DragonCrafted87 <24697493+DragonCrafted87@users.noreply.github.com>
2021-06-30 17:54:10 -0500

    update mod downloader to properly respect 2 part minecraft version and make the github api parser a bit smarter

--- root/scripts/includes/mod_downloader.py
+++ root/scripts/includes/mod_downloader.py
@@ -1,9 +1,11 @@
-#!/usr/bin/python3
+#!python
 # -*- coding: utf-8 -*-
 
-from glob import glob
+
+from argparse import ArgumentParser
 
 # System Imports
+from glob import glob
 from os import getenv
 from os import mkdir
 from os import remove as delete_file
@@ -14,27 +16,30 @@ from sys import exit as sys_exit
 from urllib.parse import quote
 
 # Local Imports
-from python_logger import create_logger
-from requests import get as http_get
-
-MODS_FOLDER = "/mnt/ramdisk/mods/"
-MODS_LIST = "/mnt/ramdisk/modlist.conf"
+# https://docs.python-requests.org/en/master/
+# pylint can't find requests for whatever reason even though its installed and works
+from requests import get as http_get  # pylint: disable=import-error
 
 
-def file_download(url):
+def file_download(url, mods_folder):
     path, url = url
-    r = http_get(url, stream=True)
+    file_stream = http_get(url, stream=True)
 
     mod_name = path[0 : search(r"\d", path).start() - 1]
-    old_mod_files = glob(f"{MODS_FOLDER}{mod_name}*")
-    for f in old_mod_files:
+    old_mod_prefix = str(PurePath(mods_folder, mod_name))
+    mod_full_path = str(PurePath(mods_folder, path))
+
+    pprint(f"Removing old version of: {old_mod_prefix}")
+    old_mod_files = glob(f"{old_mod_prefix}*")
+    for old_file in old_mod_files:
         try:
-            delete_file(f)
+            delete_file(old_file)
         except OSError:
             pass
 
-    with open(f"{MODS_FOLDER}{path}", "wb") as f:
-        f.write(r.content)
+    pprint(f"Saving updated mod: {mod_full_path}")
+    with open(mod_full_path, "wb") as new_file:
+        new_file.write(file_stream.content)
 
 
 def curseforge_parse(api_data, minecraft_version):
@@ -71,42 +76,37 @@ def curseforge_parse(api_data, minecraft_version):
 
 
 def github_parse(api_data, minecraft_version):
+
     for releases in api_data:
-        for assets in releases["assets"]:
-            link = assets.get("browser_download_url")
-            if link is not None and link.endswith("jar") and minecraft_version in link:
-                return (link[link.rfind("/") + 1 :], link)
+        if not bool(
+            releases.get("prerelease", True)
+        ) and minecraft_version in releases.get("name").split(" "):
+            for assets in releases["assets"]:
+                link = assets.get("browser_download_url")
+                if (
+                    link is not None
+                    and link.endswith("jar")
+                    and minecraft_version in link
+                ):
+                    return (link[link.rfind("/") + 1 :], link)
     return None
 
 
-def main():
-
-    logger = create_logger(PurePath(__file__).stem)
-
-    mod_list_path = getenv("MINECRAFT_MOD_LIST", f"{MODS_LIST}")
-    if mod_list_path is None:
-        logger.info("Mod List Not Found")
-        sys_exit()
-
-    with open(f"{mod_list_path}", "r") as f:
-        mod_list = [line.strip() for line in f.readlines()]
-
-    mod_list = list(filter(lambda x: x, mod_list))
-    mod_list = list(filter(lambda x: not x.startswith("#"), mod_list))
-
-    minecraft_version = getenv("MINECRAFT_VERSION", "1.17")
-    secondary_version = None
-
-    if minecraft_version.count(".") == 2:
-        secondary_version = minecraft_version[: minecraft_version.rfind(".")]
-
-    logger.info(f"Getting Mods for MineCraft Version {minecraft_version}")
-
-    logger.info("Parsing Mod List")
+def parse_mod_list(mod_list, minecraft_version, secondary_version):
+    pprint("Parsing Mod List")
     download_list = []
     for url in mod_list:
-        api_data = http_get(url)
 
+        for _ in range(5):
+            api_data = http_get(url)
+            if api_data.status_code == 200:
+                break
+
+        if api_data.status_code != 200:
+            pprint(f"Failed to get data for {url}")
+            continue
+
+        pprint(f"Getting data for {url}")
         if "github" in url:
             parse_function = github_parse
         elif "cfwidget" in url:
@@ -121,17 +121,47 @@ def main():
         if download_link is not None:
             download_list.append(download_link)
 
+    return download_list
+
+
+def main():
+
+    argument_parser = ArgumentParser()
+    argument_parser.add_argument("mod_list_path")
+    argument_parser.add_argument("mods_folder")
+    args = argument_parser.parse_args()
+
+    if args.mod_list_path is None:
+        pprint("Mod List Not Found")
+        sys_exit()
+
+    with open(f"{args.mod_list_path}", "r") as mod_list_file:
+        mod_list = [line.strip() for line in mod_list_file.readlines()]
+
+    mod_list = list(filter(lambda x: x, mod_list))
+    mod_list = list(filter(lambda x: not x.startswith("#"), mod_list))
+
+    minecraft_version = getenv("MINECRAFT_VERSION", "1.17")
+    secondary_version = None
+
+    if minecraft_version.count(".") == 2:
+        secondary_version = minecraft_version[: minecraft_version.rfind(".")]
+
+    pprint(f"Getting Mods for MineCraft Version {minecraft_version}")
+
+    download_list = parse_mod_list(mod_list, minecraft_version, secondary_version)
+
     try:
-        mkdir(MODS_FOLDER)
+        mkdir(args.mods_folder)
     except OSError:
         pass
 
-    logger.info("Downloading Mods")
-    for x in download_list:
-        file_download(x)
+    pprint("Downloading Mods")
+    for download_link in download_list:
+        file_download(download_link, args.mods_folder)
 
-    logger.info("Installed Mods")
-    logger.info(glob(f"{MODS_FOLDER}*"))
+    pprint("All Installed Mods")
+    pprint(glob(f"{str(PurePath(args.mods_folder,'*'))}"))
 
 
 if __name__ == "__main__":
